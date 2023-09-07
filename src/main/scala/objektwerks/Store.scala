70f79e4841cba752cdc5e9bf188d97a3f2e6df1b
objektwerks <objektwerks@runbox.com>
2023-09-07 10:29:54 -0400

    edited app.conf

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -2,5 +2,10 @@ package objektwerks
 
 import com.typesafe.config.Config
 
+import org.h2.jdbcx.JdbcDataSource
+
 final class Store(conf: Config):
-  
+  val ds = JdbcDataSource()
+  ds.setURL(conf.getString())
+  ds.setUser("sa")
+  ds.setPassword("")

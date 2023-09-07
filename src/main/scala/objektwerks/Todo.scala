42690065738633deac8e2411e0ac54b0b37b2e17
objektwerks <objektwerks@runbox.com>
2023-09-07 10:51:58 -0400

    enhanced todo

--- src/main/scala/objektwerks/Todo.scala
+++ src/main/scala/objektwerks/Todo.scala
@@ -1,9 +1,9 @@
 package objektwerks
 
-import com.augustnagro.magnum.{H2DbType, Id, ImmutableRepo, SqlNameMapper, Table, TableInfo}
+import com.augustnagro.magnum.{DbCodec, H2DbType, Id, ImmutableRepo, SqlNameMapper, Table, TableInfo}
 
 @Table(H2DbType, SqlNameMapper.CamelToSnakeCase)
-final case class Todo(@Id id: Int = 0, task: String)
+final case class Todo(@Id id: Int = 0, task: String) derives DbCodec
 
 object Todo:
   val repo = ImmutableRepo[Todo, Int]

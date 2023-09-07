ba9cff3316da3c08e1ee7294b046b73775a3adb4
objektwerks <objektwerks@runbox.com>
2023-09-07 16:34:50 -0400

    edited todo

--- src/main/scala/objektwerks/Todo.scala
+++ src/main/scala/objektwerks/Todo.scala
@@ -2,12 +2,11 @@ package objektwerks
 
 import com.augustnagro.magnum.{DbCodec, H2DbType, Id, Repo, SqlNameMapper, Table, TableInfo}
 
-final case class TodoBuilder(task: String) derives DbCodec
-
 @Table(H2DbType, SqlNameMapper.SameCase)
 final case class Todo(@Id id: Int, task: String) derives DbCodec
 
-final class TodoRepo extends Repo[TodoBuilder, Todo, Int]
+final case class TodoBuilder(task: String) derives DbCodec
 
-object Todo:
-  val info = TableInfo[TodoBuilder, Todo, Int]
+val info = TableInfo[TodoBuilder, Todo, Int]
+
+final class TodoRepo extends Repo[TodoBuilder, Todo, Int]

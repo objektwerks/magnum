69a4ded64268c73dd81d93f9d78b7b8262bc4207
objektwerks <objektwerks@runbox.com>
2023-09-07 13:53:26 -0400

    refactored store test to use matchers

--- src/test/scala/objektwerks/StoreTest.scala
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -3,28 +3,29 @@ package objektwerks
 import com.typesafe.config.ConfigFactory
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-class StoreTest extends AnyFunSuite:
+class StoreTest extends AnyFunSuite with Matchers:
   test("store") {
     val conf = ConfigFactory.load("test.conf")
     val store = Store(conf)
 
-    assert( store.count() == 0 )
+    store.count() shouldBe 0
 
     val todoBuilder = TodoBuilder(task = "wash car")
     var todo = store.addTodo(todoBuilder)
     println(s"*** Add Todo: $todo")
-    assert( todo.id == 1 )
+    todo.id shouldBe 1
 
     todo = todo.copy(task = "wash and dry car")
     val updated = store.updateTodo(todo)
     println(s"*** Update Todo: $todo")
-    assert(updated)
+    updated shouldBe true
 
     val count = store.count()
     println(s"*** Todo count should be 1: $count") // If count == 0, H2 and/or Magnum are failing.
 
     val todos = store.listTodos()
     println(s"*** List Todos: ${todos.toString}")
-    assert( todos.length == 1 )
+    todos.length shouldBe 1
   }

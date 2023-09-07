d4d8132ab3838b85fb9a65dfbf23b85ea574ba14
objektwerks <objektwerks@runbox.com>
2023-09-07 10:30:53 -0400

    enhanced store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -6,6 +6,6 @@ import org.h2.jdbcx.JdbcDataSource
 
 final class Store(conf: Config):
   val ds = JdbcDataSource()
-  ds.setURL(conf.getString())
-  ds.setUser("sa")
-  ds.setPassword("")
+  ds.setURL(conf.getString("url"))
+  ds.setUser(conf.getString("user"))
+  ds.setPassword(conf.getString("password"))

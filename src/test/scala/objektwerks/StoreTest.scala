0ab8842082a36c6fcb951c998d0f7a2442ee9d22
objektwerks <objektwerks@runbox.com>
2023-09-08 05:38:26 -0400

    refactored store test

--- src/test/scala/objektwerks/StoreTest.scala
+++ src/test/scala/objektwerks/StoreTest.scala
@@ -9,17 +9,16 @@ class StoreTest extends AnyFunSuite with Matchers:
   test("store") {
     val conf = ConfigFactory.load("test.conf")
     val store = Store(conf)
-    var todo = Todo(0, "")
 
     store.count() shouldBe 0
 
     val todoBuilder = TodoBuilder(task = "wash car")
-    todo = store.addTodo(todoBuilder)
+    val todo = store.addTodo(todoBuilder)
     println(s"*** Add Todo: $todo")
     todo.id shouldBe 1
 
-    todo = todo.copy(task = "wash and dry car")
-    val updated = store.updateTodo(todo)
+    val updatedTodo = todo.copy(task = "wash and dry car")
+    val updated = store.updateTodo(updatedTodo)
     println(s"*** Update Todo: $todo")
     updated shouldBe true
 

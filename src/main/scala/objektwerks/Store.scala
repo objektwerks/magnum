86a75166cf6f89528bf79652f980d0be71af8a13
objektwerks <objektwerks@runbox.com>
2023-09-12 14:26:28 -0400

    enhanced store

--- src/main/scala/objektwerks/Store.scala
+++ src/main/scala/objektwerks/Store.scala
@@ -1,6 +1,6 @@
 package objektwerks
 
-import com.augustnagro.magnum.transact
+import com.augustnagro.magnum.{DbTx, transact}
 import com.typesafe.config.Config
 
 import java.nio.file.Files
@@ -14,9 +14,9 @@ import scala.util.Using
 
 final class Store(conf: Config):
   val ds = JdbcDataSource()
-  val url = conf.getString("ds.url")
-  val user = conf.getString("ds.user")
-  val password = conf.getString("ds.password")
+  ds.setURL( conf.getString("ds.url") )
+  ds.setUser( conf.getString("ds.user") )
+  ds.setPassword( conf.getString("ds.password") )
 
   val success = Using.Manager(use =>
     val connection = use( ds.getConnection )
@@ -32,19 +32,19 @@ final class Store(conf: Config):
   private def withRepeatableRead(connection: Connection): Unit =
     connection.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ)
 
-  def count(): Long =
+  def count()(using DbTx): Long =
     transact(ds, withRepeatableRead):
       repo.count
 
-  def addTodo(todo: TodoBuilder): Todo =
+  def addTodo(todo: TodoBuilder)(using DbTx): Todo =
     transact(ds):
       repo.insertReturning(todo)
 
-  def updateTodo(todo: Todo): Boolean =
+  def updateTodo(todo: Todo)(using DbTx): Boolean =
     transact(ds):
       repo.update(todo)
       true
 
-  def listTodos(): Vector[Todo] =
+  def listTodos()(using DbTx): Vector[Todo] =
     transact(ds, withRepeatableRead):
       repo.findAll

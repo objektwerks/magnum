34fdcd6daba56b07c4f03ca42d3544a09350a5ad
objektwerks <objektwerks@runbox.com>
2023-09-15 11:10:17 -0400

    refactored todo orderby

--- src/main/scala/objektwerks/Todo.scala
+++ src/main/scala/objektwerks/Todo.scala
@@ -4,6 +4,13 @@ import com.augustnagro.magnum.{DbCodec, H2DbType, Id, NullOrder, Repo, SortOrder
 
 import java.time.Instant
 
+enum OrderBy(spec: Spec[Todo]):
+  case task extends OrderBy(Todo.orderByTaskSpec)
+  case created extends OrderBy(Todo.orderByCreatedSpec)
+  case completed extends OrderBy(Todo.orderByCompletedSpec)
+
+  def todoSpec(): Spec[Todo] = spec
+
 object Todo:
   given taskOrdering: Ordering[Todo] = Ordering.by[Todo, String](t => t.task)
   given createdOrdering: Ordering[Todo] = Ordering.by[Todo, Long](t => t.created).reverse
@@ -17,13 +24,6 @@ object Todo:
 
   val orderByCompletedSpec = Spec[Todo]
     .orderBy("completed", SortOrder.Asc, NullOrder.Last)
-
-  enum OrderBy(spec: Spec[Todo]):
-    case task extends OrderBy(orderByTaskSpec)
-    case created extends OrderBy(orderByCreatedSpec)
-    case completed extends OrderBy(orderByCompletedSpec)
-
-    def todoSpec(): Spec[Todo] = spec
 
   val info = TableInfo[TodoBuilder, Todo, Int]
 

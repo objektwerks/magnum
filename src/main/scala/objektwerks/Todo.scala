703c294c578d1e1f7a772f6c6b7798de12eb4761
objektwerks <objektwerks@runbox.com>
2023-09-15 08:07:18 -0400

    edited todo

--- src/main/scala/objektwerks/Todo.scala
+++ src/main/scala/objektwerks/Todo.scala
@@ -5,9 +5,14 @@ import com.augustnagro.magnum.{DbCodec, H2DbType, Id, Repo, SqlNameMapper, Table
 import java.time.Instant
 
 @Table(H2DbType, SqlNameMapper.SameCase)
-final case class Todo(@Id id: Int, task: String, created: Long, completed: Long) derives DbCodec
+final case class Todo(@Id id: Int,
+                      task: String,
+                      created: Long,
+                      completed: Long) derives DbCodec
 
-final case class TodoBuilder(task: String, created: Long = Instant.now.getEpochSecond, completed: Long = 0) derives DbCodec
+final case class TodoBuilder(task: String,
+                             created: Long = Instant.now.getEpochSecond,
+                             completed: Long = 0) derives DbCodec
 
 final class TodoRepo extends Repo[TodoBuilder, Todo, Int]
 

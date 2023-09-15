dc160cd600e04e5462a35cca1ed5852f6229ab3b
objektwerks <objektwerks@runbox.com>
2023-09-15 08:17:58 -0400

    enhanced todo

--- src/main/scala/objektwerks/Todo.scala
+++ src/main/scala/objektwerks/Todo.scala
@@ -9,6 +9,8 @@ object Todo:
   given createdOrdering: Ordering[Todo] = Ordering.by[Todo, Long](t => t.created).reverse
   given completedOrdering: Ordering[Todo] = Ordering.by[Todo, Long](t => t.completed)
 
+  def epochSecond(): Long = Instant.now.getEpochSecond
+
 @Table(H2DbType, SqlNameMapper.SameCase)
 final case class Todo(@Id id: Int,
                       task: String,
@@ -16,7 +18,7 @@ final case class Todo(@Id id: Int,
                       completed: Long) derives DbCodec
 
 final case class TodoBuilder(task: String,
-                             created: Long = Instant.now.getEpochSecond,
+                             created: Long = Todo.epochSecond(),
                              completed: Long = 0) derives DbCodec
 
 final class TodoRepo extends Repo[TodoBuilder, Todo, Int]

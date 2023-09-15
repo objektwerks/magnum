fc2ed2eec4445a67fe3bf1a847a4e91ea5c5311c
objektwerks <objektwerks@runbox.com>
2023-09-15 08:05:55 -0400

    edited ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -2,6 +2,6 @@ drop table todo if exists;
 create table todo (
   id int primary key auto_increment,
   task varchar(128) not null,
-  create BIGINT not null,
+  created BIGINT not null,
   completed BIGINT not null
 );
